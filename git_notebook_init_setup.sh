c8c76ea24c930399c3eb42918dbc29c26b30557b
PernilleBrams (Ucloud) <pernille@brams.dk>
2023-09-13 16:50:15 +0200

    just a few tryout things

--- git_notebook_init_setup.sh
+++ git_notebook_init_setup.sh
@@ -4,11 +4,14 @@ git config --global user.name "PernilleBrams (Ucloud)"
 
 # - initialize a notebook
 
+# upgrade pip
+pip install --upgrade pip
+
 # install ipykernel
 pip install ipykernel
 
-# tell it that we have the nlp-e23 kernel - pls use
+# tell it that we have the nlp-e23 kernel - pls use (this doesn't seem to do anything)
 python3 -m ipykernel install --user --name=nlp-e23
 
-# install requirements
-pip install -r requirements.txt
+# install requirements (this doesn't seem to work either because it can't access the files)
+# pip install -r requirements.txt
